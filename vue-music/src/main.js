2d3bb9dd3d4c890f471a71ee145e4e78b1f6a1ca
githubdelegate <ostmail@163.com>
2018-06-28 10:02:17 +0800

    init vue music project

--- vue-music/src/main.js
+++ vue-music/src/main.js
@@ -2,14 +2,13 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+// import router from './router'
+import 'common/stylus/index.styl'
 
-Vue.config.productionTip = false
+// Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+  render: h => h(App)
 })
