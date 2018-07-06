12e05ff1d73ca244df3726d2076bf697f9227b53
githubdelegate <ostmail@163.com>
2018-07-06 21:17:31 +0800

    add scroll vuew

---
+++ vue-music/src/base/scroll/scroll.vue
@@ -0,0 +1,63 @@
+<template>
+  <div ref="wrapper">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import BScroll from 'better-scroll'
+
+export default {
+  props: {
+    probeType: {
+      type: Number,
+      default: 1
+    },
+    click: {
+      type: Boolean,
+      default: true
+    },
+    data: {
+      type: Array,
+      default: null
+    }
+  },
+  name: 'Scorll',
+  mounted() {
+    setTimeout(() => {
+      this._initScorll()
+    }, 20)
+  },
+  methods: {
+    _initScorll() {
+      if (!this.$refs.wrapper) {
+        return
+      }
+      this.scroll = new BScroll(this.$refs.wrapper, {
+        probeType: this.probeType,
+        click: this.click
+      })
+    },
+    enable() {
+      this.scroll && this.scroll.enable()
+    },
+    disable() {
+      this.scroll && this.scroll.disable()
+    },
+    refresh() {
+      this.scroll && this.scroll.refresh()
+    }
+  },
+  watch: {
+    data() {
+      setTimeout(() => {
+        console.log(' watch data change ')
+        this.refresh()
+      }, 20)
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+</style>
--- vue-music/src/components/recommend/recommend.vue
+++ vue-music/src/components/recommend/recommend.vue
@@ -1,16 +1,32 @@
 <template>
   <div class="recommend">
-    <div class="recommend-content">
-      <div class="slider-wrapper">
-        <slider v-if="recommends.length">
-          <div v-for="item in recommends" :key="item">
-            <a :href="item.linkUrl">
-            <img :src="item.picUrl" alt="">
-            </a>
-          </div>
-        </slider>
+    <scroll class="recommend-content">
+      <div>
+        <div class="slider-wrapper">
+          <slider v-if="recommends.length">
+            <div v-for="item in recommends" :key="item">
+              <a :href="item.linkUrl">
+              <img :src="item.picUrl" alt="">
+              </a>
+            </div>
+          </slider>
       </div>
-    </div>
+      <div class="recommend-list">
+        <h1 class="list-title">热门歌单推荐</h1>
+        <ul class="list-content">
+          <li v-for="item in dissList" :key="item" class="list-item">
+            <div class="icon">
+              <img :src="item.imgurl" alt="" width="60" height="60">
+            </div>
+            <div class="text">
+              <h2 class="name" v-html="item.creator.name"></h2>
+              <p class="desc" v-html="item.dissname"></p>
+            </div>
+          </li>
+        </ul>
+      </div>
+      </div>
+    </scroll>
   </div>
 </template>
 
@@ -18,20 +34,20 @@
 import Slider from 'base/slider/slider'
 import {getRecommend, getDissList} from 'api/recommend'
 import {ERR_OK} from 'api/config'
+import Scroll from 'base/scroll/scroll'
 export default {
   data() {
     return {
-      recommends: []
+      recommends: [],
+      dissList: []
     }
   },
   created() {
-    console.log('BENGIN  get recommend')
     this._getRecommend()
     this._getDissList()
   },
   methods: {
     _getRecommend() {
-      console.log('BENGIN  get recommend')
       getRecommend().then((res) => {
         if (res.code === ERR_OK) {
           console.log(res)
@@ -42,18 +58,51 @@ export default {
     _getDissList() {
       getDissList().then((res) => {
         if (res.code === ERR_OK) {
-          console.log(res)
+          this.dissList = res.data.list
+          console.log(res.data.list)
         }
       })
     }
   },
   components: {
-    Slider
+    Slider,
+    Scroll
   }
 }
 
 </script>
 
 <style lang="stylus" scoped rel="stylesheet/stylus">
-
+@import "~common/stylus/variable"
+.recommend-list
+  position relative
+  margin-top 10px
+  .list-title
+    text-align center
+    color $color-theme
+    line-height 65px
+    height 65px
+    font-size $font-size-medium
+  .list-content
+    .list-item
+      align-items center
+      display flex
+      padding 0 20px 20px 20px
+      .icon
+        flex  0 0 60px
+        width 60px
+        padding-right 20px
+      .text
+        display flex
+        flex-direction column
+        justify-content center
+        flex 1
+        line-height 20px
+        overflow hidden
+        font-size $font-size-medium
+        .name
+          margin-bottom 10px
+          color $color-text
+        .desc
+          color $color-text-d
 </style>
