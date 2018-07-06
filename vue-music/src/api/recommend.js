5d8f8a2d565899f5627f9872f12074db52fa013b
githubdelegate <ostmail@163.com>
2018-07-06 14:11:32 +0800

    修改参数

--- vue-music/src/api/recommend.js
+++ vue-music/src/api/recommend.js
@@ -21,37 +21,36 @@ export function getRecommend() {
 export function getDissList() {
   // https://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_by_tag.fcg'
   const url = '/api/getDissList'
-  // const data = Object.assign({}, commonParams, {
-  //   platform: 'yqq',
-  //   hostUin: 0,
-  //   sin: 0,
-  //   ein: 29,
-  //   sortId: 5,
-  //   needNewCode: 0,
-  //   categoryId: 10000000,
-  //   rnd: Math.random(),
-  //   format: 'json',
-  //   g_tk: 5381
-  // })
-  // return jsonp(url, data, options)
-  const data = {
-    picmid: 1,
-    rnd: Math.random(),
-    g_tk: 5381,
-    jsonpCallback: 'getPlaylist',
-    loginUin: '0',
-    hostUin: 0,
-    format: 'json',
-    inCharset: 'utf8',
-    outCharse: 'utf-8',
-    notice: 0,
+  const data = Object.assign({}, commonParams, {
     platform: 'yqq',
+    hostUin: 0,
+    sin: 0,
+    ein: 29,
+    sortId: 5,
     needNewCode: 0,
     categoryId: 10000000,
-    sortId: 5,
-    sin: 0,
-    ein: 29
-  }
+    rnd: Math.random(),
+    format: 'json'
+  })
+  // return jsonp(url, data, options)
+  // const data = {
+  //   picmid: 1,
+  //   rnd: Math.random(),
+  //   g_tk: 5381,
+  //   // jsonpCallback: 'getPlaylist',
+  //   loginUin: '0',
+  //   hostUin: 0,
+  //   format: 'json',
+  //   inCharset: 'utf8',
+  //   outCharse: 'utf-8',
+  //   notice: 0,
+  //   platform: 'yqq',
+  //   needNewCode: 0,
+  //   categoryId: 10000000,
+  //   sortId: 5,
+  //   sin: 0,
+  //   ein: 29
+  // }
   return axios.get(url, {
     params: data
   }).then((res) => {
