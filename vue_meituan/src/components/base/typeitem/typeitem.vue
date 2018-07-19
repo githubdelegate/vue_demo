6dbbfb26f7c5720981e657e4a0b688286355b48e
githubdelegate <ostmail@163.com>
2018-07-19 14:31:57 +0800

    实现分类按钮

---
+++ vue_meituan/src/components/base/typeitem/typeitem.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="type-item" @click="toList">
+    <img :src="ico" alt="">
+    <span>{{ txt }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    ico: {
+      type: String,
+      default: ''
+    },
+    txt: {
+      type: String,
+      default: ''
+    }
+  },
+  methods: {
+    toList () {
+      this.$emit('toList')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.type-item {
+  float: left;
+  width: 25%;
+  text-align: center;
+  padding-top: 14px;
+  img {
+    display: block;
+    width: 50px;
+    margin: 0 auto 12px;//设置块级元素的 width 可以防止它从左到右撑满整个容器。
+    // 然后你就可以设置左右外边距为 auto 来使其水平居中。元素会占据你所指定的宽度，然后剩余的宽度会一分为二成为左右外边距。
+  }
+  span {
+    display: block;
+    line-height: 14px; // 表示行高
+    font-size: 14px; // 表示文字大小
+    color: #2f2f2f;
+  }
+}
+</style>
--- vue_meituan/src/components/index/index.vue
+++ vue_meituan/src/components/index/index.vue
@@ -7,15 +7,21 @@
       </mt-swipe-item>
     </mt-swipe>
   </div>
+  <div class="types">
+    <type-item v-for="type in types" :key="type" :ico="type.ico" :txt="type.txt"></type-item>
+  </div>
   <tabbar></tabbar>
 </div>
 </template>
 
 <script>
 import Tabbar from 'base/tabbar/tabbar'
+import TypeItem from 'base/typeitem/typeitem'
+
 export default {
   components: {
-    Tabbar
+    Tabbar,
+    TypeItem
   },
   data () {
     return {
@@ -28,6 +34,43 @@ export default {
         },
         {
           pic: require('./img/swipe/3.jpg')
+        },
+        {
+          pic: require('./img/swipe/bannertemp.e8a6fa63.jpg')
+        }
+      ],
+      types: [
+        {
+          ico: require('./img/types/types (0).png'),
+          txt: '美食'
+        },
+        {
+          ico: require('./img/types/types (1).png'),
+          txt: '美团超市'
+        },
+        {
+          ico: require('./img/types/types (2).png'),
+          txt: '生鲜果蔬'
+        },
+        {
+          ico: require('./img/types/types (3).png'),
+          txt: '甜点饮品'
+        },
+        {
+          ico: require('./img/types/types (4).png'),
+          txt: '正餐优选'
+        },
+        {
+          ico: require('./img/types/types (5).png'),
+          txt: '美团专送'
+        },
+        {
+          ico: require('./img/types/types (6).png'),
+          txt: '能量西餐'
+        },
+        {
+          ico: require('./img/types/types (7).png'),
+          txt: '精品小吃'
         }
       ]
     }
