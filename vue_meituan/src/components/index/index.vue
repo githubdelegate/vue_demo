b7a5956f9a700104a6f4b347ad178c17b8a0981c
githubdelegate <ostmail@163.com>
2018-07-19 10:26:31 +0800

    实现 tabbar  主要注意点是 reset.css

---
+++ vue_meituan/src/components/index/index.vue
@@ -0,0 +1,40 @@
+<template>
+<div class="index">
+  <tabbar></tabbar>
+</div>
+</template>
+
+<script>
+import Tabbar from 'base/tabbar/tabbar'
+export default {
+  components: {
+    Tabbar
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~@/assets/scss/const.scss';
+@import '~@/assets/scss/mixin.scss';
+.index {
+  .slider {
+    height: 170px;
+    font-size: 30px;
+    text-align: center;
+    overflow: hidden;
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+  .types {
+    overflow: hidden;
+    padding-bottom: 20px;
+    background-color: #fff;
+  }
+  .nearby {
+    margin-bottom: 50px;
+    background-color: #fff;
+  }
+}
+</style>
--- vue_meituan/src/main.js
+++ vue_meituan/src/main.js
@@ -3,8 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 将全局样式文件写在 main.js
+import '@/assets/scss/reset.scss'
+import '@/assets/scss/iconfont.scss'
+import '@/assets/ali-fonts/iconfont.css'
+// Mint UI
+import Mint from 'mint-ui'
+import 'mint-ui/lib/style.css'
 
 Vue.config.productionTip = false
+Vue.use(Mint)
 
 /* eslint-disable no-new */
 new Vue({
